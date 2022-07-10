c5763a090dabd40b2e2396a410ab3fb3a661da54
augustocrmg <augusto.duraes33@gmail.com>
2022-07-09 21:23:53 -0300

    Automatic Commit

--- commit.sh
+++ commit.sh
@@ -4,6 +4,12 @@ PATH_REPO="D:/Software/AutoCommit/commit-script"
 
 cd $PATH_REPO
 pwd
+
+# Fetching and Pulling just to make sure there isn t something new in repo
+git fetch
+git pull
+
+# Commiting files
 git add . && \
 git add -u && \
 git commit -m "Automatic Commit " && \
